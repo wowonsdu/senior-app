dff87c5c0ed141f1863245bce87aecf5a97bdd0f
Grzegorz Emanowicz <grzegorz.emanowicz@gmail.com>
2026-02-19 18:09:06 +0100

    Add role select screen layout and screenshot

--- ui/src/main/kotlin/zdrowy/senior/io/ui/auth/RoleSelectFragment.kt
+++ ui/src/main/kotlin/zdrowy/senior/io/ui/auth/RoleSelectFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import zdrowy.senior.io.ui.R
 import zdrowy.senior.io.ui.databinding.FragmentRoleSelectBinding
 
 class RoleSelectFragment : Fragment() {
@@ -17,6 +19,12 @@ class RoleSelectFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentRoleSelectBinding.inflate(inflater, container, false)
+        binding.roleSelectPatientCard.setOnClickListener {
+            findNavController().navigate(R.id.action_roleSelect_to_patientLogin)
+        }
+        binding.roleSelectCaregiverCard.setOnClickListener {
+            findNavController().navigate(R.id.action_roleSelect_to_caregiverLogin)
+        }
         return binding.root
     }
 
