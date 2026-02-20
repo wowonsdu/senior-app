e9e8d7a3fc9f9415c1e60a53c0800324839d012f
Grzegorz Emanowicz <grzegorz.emanowicz@gmail.com>
2026-02-20 16:29:16 +0100

    Run Firestore tasks off main thread

--- data/src/main/kotlin/zdrowy/senior/io/data/firestore/RxTasks.kt
+++ data/src/main/kotlin/zdrowy/senior/io/data/firestore/RxTasks.kt
@@ -1,19 +1,14 @@
 package zdrowy.senior.io.data.firestore
 
 import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
 
 internal fun Task<*>.toCompletable(): Completable {
-    return Completable.create { emitter ->
-        addOnSuccessListener { if (!emitter.isDisposed) emitter.onComplete() }
-        addOnFailureListener { error -> if (!emitter.isDisposed) emitter.onError(error) }
-    }
+    return Completable.fromAction { Tasks.await(this) }
 }
 
 internal fun <T : Any> Task<T>.toSingle(): Single<T> {
-    return Single.create { emitter ->
-        addOnSuccessListener { result -> if (!emitter.isDisposed) emitter.onSuccess(result) }
-        addOnFailureListener { error -> if (!emitter.isDisposed) emitter.onError(error) }
-    }
+    return Single.fromCallable { Tasks.await(this) }
 }
